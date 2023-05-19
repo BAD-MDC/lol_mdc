357560af172f1cda1eebce84789788543dfaa1aa
crAy <crAyCoding@gmail.com>
2023-05-19 21:35:08 +0900

    Add wrtieJSON python file

---
+++ LOL/lolData.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+'''
+Data Dragon으로 부터 최신 version의 챔피언 정보 열람
+차후 업데이트 예정
+'''
+
+#가장 최신 버전 정보 반환
+def latest_version() : 
+     version_url = "https://ddragon.leagueoflegends.com/api/versions.json"
+     version_data = requests.get(version_url).json()
+     latest_version = str(version_data[0])
+     return latest_version
+
+#챔피언 JSON 파일 반환
+def champions() :
+     champion_data_url = "http://ddragon.leagueoflegends.com/cdn/" + latest_version() + "/data/en_US/champion.json"
+     champion_data = requests.get(champion_data_url).json()
+     return champion_data
+
+
+#챔피언 이름에 따른 결과값 반환
+
+#챔피언 레벨 입력값에 따라 정보 반환
---
+++ LOL/writeChampionJSON.py
@@ -0,0 +1,12 @@
+import requests
+import json
+from lolData import champions
+
+#LOL 폴더에 챔피언 정보 JSON 파일 생성
+def writeChampionJSON() :
+
+    with open('LOL/champion.json', 'w') as convert_file:
+        json.dump(champions(), convert_file)
+
+
+writeChampionJSON()
