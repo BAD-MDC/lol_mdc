4183dee2a236383e995c3b9af34533beeeae75b5
crAy <crAyCoding@gmail.com>
2023-05-21 23:16:23 +0900

    Search champion data by name

--- LOL/lolData.py
+++ LOL/lolData.py
@@ -15,18 +15,22 @@ def latest_version() :
 
 #챔피언 JSON 파일 반환
 def champions() :
-     champion_data_url = "http://ddragon.leagueoflegends.com/cdn/" + latest_version() + "/data/ko_KR/champion.json"
-     champion_data = requests.get(champion_data_url).json()
-     return champion_data
+     champions_data_url = "http://ddragon.leagueoflegends.com/cdn/" + latest_version() + "/data/ko_KR/champion.json"
+     champions_data = requests.get(champions_data_url).json()
+     return champions_data
 
 
 #챔피언 이름에 따른 결과값 반환
+def champion(champion_name) :
+     champion_data_url = "http://ddragon.leagueoflegends.com/cdn/" + latest_version() + "/data/ko_KR/champion/" + champion_name + ".json"
+     champion_data = requests.get(champion_data_url).json()
+     return champion_data
 
 #챔피언 레벨 입력값에 따라 정보 반환
 
 #챔피언 목록 반환
-def champion_name(champion_kor_name) :
+def champion_eng_name(champion_kor_name) :
      for champion in champions()["data"] :
           if champions()["data"][champion]["name"] == champion_kor_name :
-               return(champions()["data"][champion])
-     return "ERROR"     
+               return(champions()["data"][champion]["id"])
+     return "ERROR"
