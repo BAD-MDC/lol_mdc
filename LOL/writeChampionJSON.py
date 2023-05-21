da0667ad0b01693f5e84204d34234ee0eed03547
crAy <crAyCoding@gmail.com>
2023-05-22 01:43:50 +0900

    Show champion stats by level

--- LOL/writeChampionJSON.py
+++ LOL/writeChampionJSON.py
@@ -6,7 +6,6 @@ from lolData import champion_eng_name
 
 #LOL 폴더에 전체 챔피언 정보 JSON 파일 생성
 def writeChampionsJSON() :
-
     with open('LOL/champion_data/champion.json', 'w') as convert_file:
         json.dump(champions(), convert_file, ensure_ascii = False)
 
@@ -16,6 +15,3 @@ def writeChampionJSON(champion_kor_name) :
     champion_json_file_name = 'LOL/champion_data/' +  champion_name + '.json'
     with open(champion_json_file_name, 'w') as convert_file:
         json.dump(champion(champion_name), convert_file, ensure_ascii = False)
-
-writeChampionsJSON()
-#writeChampionJSON("루시안")
